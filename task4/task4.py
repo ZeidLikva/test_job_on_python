5d15eae999dcd5aa8abf3b1a3f303e4827df6567
Inga-Viktoriia Kuksova <na.yada1990@gmail.com>
2020-11-18 23:34:33 +0300

    add task4

---
+++ task4/task4.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+from sys import argv
+import sys
+
+def compare_str(str1, str2):
+    j = 0
+    i = 0
+    while i != (len(str2)-1):
+        while (str2[i] == '*'):
+            if i != (len(str2)-1):
+                i += 1
+                continue
+            else:
+                return 'OK'
+        if (str1[j] != str2[i]):
+            return 'KO'
+        if ((j == (len(str1)-1)) and ((i != (len(str2)-1)) and (str2[i+1] != '*'))) or ((j != (len(str1)-1)) and (i == (len(str2)-1))):
+            return 'KO'
+        if j != (len(str1)-1):
+            j += 1
+        i += 1
+    return 'OK'
+
+
+def main(argv):
+    if len(argv) == 2:
+        str1 = argv[0]
+        str2 = argv[1]
+        print(compare_str(str1, str2))
+
+
+main(sys.argv[1:])
